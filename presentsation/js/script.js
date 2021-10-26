9afdbfdb31b3b4201ed664a222abda47a34082a1
Stephan138 <80909585+Stephan138@users.noreply.github.com>
2021-10-26 21:23:57 +0300

    Update script.js

--- presentsation/js/script.js
+++ presentsation/js/script.js
@@ -47,15 +47,17 @@ function addSlide(Editor, Slide) {}
 /**
  * @param {Editor} Editor
  * @param {Slide} Slide
+ * @param {number} randomId
  */
-function del(editor, Slide) {}
+function del(editor, Slide, randomId) {}
 
 /**
  * @param {number} id
  * @param {string} colour
+ * @param {string} directory
  * @return {Slide}
  */
-function changeBackgroundSline(id, colour) {}
+function changeBackgroundSline(id, colour, directory) {}
 
 /**
  * @param {Editor} Editor
@@ -71,4 +73,41 @@ function addText(Editor, Text) {}
  * @return {Editor}
  */
 function changeText(id, size, font) {}
+
+/**
+ * @param {Editor} Editor
+ * @param {string} URL
+ * @return {Editor}
+ */
+function addImg(Editor, URL) {}
+
+/**
+ * @param {Editor} Editor
+ * @param {string} type
+ * @return {Editor}
+ */
+function addPrimitive(Editor, type) {}
+
+/**
+ * @param {Editor} Editor
+ * @param {string} colourback
+ * @param {string} colourline
+ * @return {Editor}
+ */
+function editPrimitive(Editor, colourback, colourline) {}
+
+/**
+ * @param {show} show 
+ * @param {presentation} presentation
+ */
+function showmode(show, presentation) {}
+
+
+/**
+ * @param {Editor} Editor
+ * @param {string} position 
+ * @param {number} elementId
+ * @return {Editor}
+ */
+function selectElements(Editor, position, elementId) {}
 
